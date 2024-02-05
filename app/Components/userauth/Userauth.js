e8e2b3f62e508ce24c8fe19765ec2eaace43e5e9
WahidX <wahidx93@gmail.com>
2024-02-06 01:35:53 +0530

    logo done

--- app/Components/userauth/Userauth.js
+++ app/Components/userauth/Userauth.js
@@ -1,40 +1,41 @@
-"use client"
-import {useState } from 'react'
-import {motion} from "framer-motion";
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
 import Nav from "./Navbar";
 import Cont from "./input";
 
-export default function Auth(){
-	const [login,setIslogin]=useState(true);
-	const [create,setCreate]=useState(false);
+export default function Auth() {
+	const [login, setIslogin] = useState(true);
+	const [create, setCreate] = useState(false);
 
-	const handleLoginclick=()=>{
+	const handleLoginclick = () => {
 		setIslogin(true);
 		setCreate(false);
-	}
+	};
 
-	const handleCreateclick=()=>{
+	const handleCreateclick = () => {
 		setCreate(true);
 		setIslogin(false);
 	};
 
-	return(
-		<motion.div 
-		initial={{
-			width:'1vw'
-		}}
-		transition={{
-			delay:1,
-			type:'spring',
-            stiffness:100,
-            damping:10
-		}}
-		animate={{
-            width:'40vw'
-        }}
-		id="auth-container">
+	return (
+		<motion.div
+			initial={{
+				width: "1vw",
+			}}
+			transition={{
+				delay: 1,
+				type: "spring",
+				stiffness: 100,
+				damping: 16,
+			}}
+			animate={{
+				width: "40vw",
+			}}
+			id="auth-container"
+		>
 			<Nav login={handleLoginclick} isclick={login} create={handleCreateclick}></Nav>
-			<Cont login={login} create={create}  ></Cont>
+			<Cont login={login} create={create}></Cont>
 		</motion.div>
-	)
+	);
 }
--- app/Logo.js
+++ app/Logo.js
@@ -1,24 +1,36 @@
-import {motion} from 'framer-motion'
-export default function Logo(){
-    const list=["R","I","A","L"]
-    return(
-        <div id="logo">
-            <motion.div initial={{y:-600}}transition={{delay:1,
-			type:'spring',
-            stiffness:100,
-            damping:14}} animate={{y:0}}>R</motion.div>
-            <motion.div initial={{y:600}}transition={{delay:1,
-			type:'spring',
-            stiffness:100,
-            damping:14}} animate={{y:0}}>I</motion.div>
-            <motion.div initial={{y:-600}}transition={{delay:1,
-			type:'spring',
-            stiffness:100,
-            damping:14}} animate={{y:0}}>A</motion.div>
-            <motion.div initial={{y:600}}transition={{delay:1,
-			type:'spring',
-            stiffness:100,
-            damping:14}} animate={{y:0}}>L</motion.div>
-        </div>
-    )
+import { motion } from "framer-motion";
+export default function Logo() {
+	const list = ["R", "I", "A", "L"];
+	return (
+		<div id="logo">
+			<motion.div
+				initial={{ y: -600 }}
+				transition={{ delay: 1, type: "spring", stiffness: 100, damping: 19 }}
+				animate={{ y: 0 }}
+			>
+				R
+			</motion.div>
+			<motion.div
+				initial={{ y: 600 }}
+				transition={{ delay: 1, type: "spring", stiffness: 100, damping: 19 }}
+				animate={{ y: 0 }}
+			>
+				I
+			</motion.div>
+			<motion.div
+				initial={{ y: -600 }}
+				transition={{ delay: 1, type: "spring", stiffness: 100, damping: 19 }}
+				animate={{ y: 0 }}
+			>
+				A
+			</motion.div>
+			<motion.div
+				initial={{ y: 600 }}
+				transition={{ delay: 1, type: "spring", stiffness: 100, damping: 19 }}
+				animate={{ y: 0 }}
+			>
+				L
+			</motion.div>
+		</div>
+	);
 }
