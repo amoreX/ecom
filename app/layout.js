b1c5da3ad4aa8f80ed1c4f76b918aecf832d9ada
WahidX <wahidx93@gmail.com>
2024-02-05 16:15:40 +0530

    clearing junk

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ecommerce",
@@ -10,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
